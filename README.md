83b54004dba69e1352feebd0dff05ebae0a586e5
Justin Love <git@JustinLove.name>
2016-01-29 11:46:15 -0600

    overwrite card storage

---
+++ README.md
@@ -0,0 +1,3 @@
+# GW Unlock Loadouts
+
+Permanently unlocks all loadouts in Galactic War. You may remove the mod once unlocked.
---
+++ ui/mods/gw_unlock_loadouts/start.js
@@ -0,0 +1,1 @@
+localStorage['gw_bank'] = '{"startCards":[{"id":"gwc_start_vehicle"},{"id":"gwc_start_air"},{"id":"gwc_start_orbital"},{"id":"gwc_start_bot"},{"id":"gwc_start_artillery"},{"id":"gwc_start_subcdr"},{"id":"gwc_start_combatcdr"},{"id":"gwc_start_allfactory"}]}'
